b509822ea1a0f762e7dcacae4e9c2eb8a7204e08
Eli Rykoff <erykoff@stanford.edu>
2022-08-23 11:01:17 -0700

    Update build.sh and reqs.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
+# Get an updated config.sub and config.guess
+chmod +w ./config.guess ./config.sub
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./
+
+autoreconf -ivf
+
 ./configure --prefix=$PREFIX
 make
 make install
